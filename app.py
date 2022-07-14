06cca6aca301d3d02c47f6030bf71d5d4f6e39cb
Rohan Nirwan <rohan.nirwan@think360.ai>
2022-07-14 15:11:01 +0530

    heroku push

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from datetime import datetime
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 @app.route('/')
 def homepage():
@@ -9,6 +10,13 @@ def homepage():
 @app.route('/cloneTest')
 def clonepage():
     return "clone Test"
+
+@app.route('/getName', methods=['GET'])
+def getName():
+    name = {"name": "Nishant Gada"}
+    result = jsonify(name)
+    return result
+    
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
